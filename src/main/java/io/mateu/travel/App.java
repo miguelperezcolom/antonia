85da0318fd0e4f456c8625158ef61dafdcf19ac8
miguelperezcolom <miguelperezcolom@gmail.com>
2017-02-07 20:11:53 +0100

    Initial commit

---
+++ src/main/java/io/mateu/travel/App.java
@@ -0,0 +1,13 @@
+package io.mateu.travel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
