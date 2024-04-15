bf82b0770b1b3e4f0defceecaa2a3fc9a0afd791
Miguel Pérez <miguel.perez@sembo.se>
2024-04-15 17:39:55 +0200

    add some value objects

--- backoffice/modulith/src/main/java/io/mateu/travel/backoffice/booking/domain/model/aggregates/booking/Booking.java
+++ backoffice/modulith/src/main/java/io/mateu/travel/backoffice/booking/domain/model/aggregates/booking/Booking.java
@@ -1,18 +1,35 @@
 package io.mateu.travel.backoffice.booking.domain.model.aggregates.booking;
 
+import io.mateu.travel.backoffice.booking.domain.model.aggregates.booking.valueobjects.Amount;
 import io.mateu.travel.backoffice.booking.domain.model.aggregates.booking.valueobjects.BookingId;
+import io.mateu.travel.backoffice.booking.domain.model.aggregates.booking.valueobjects.LeadName;
+import io.mateu.travel.backoffice.booking.domain.model.aggregates.booking.valueobjects.ServiceDescription;
 import io.mateu.travel.backoffice.shared.domain.AggregateRoot;
+import lombok.Getter;
 
+@Getter
 public class Booking implements AggregateRoot {
 
     private final BookingId id;
+    private final LeadName leadName;
+    private final ServiceDescription serviceDescription;
+    private final Amount salePrice;
 
-
-    public Booking(BookingId id) {
+    public Booking(BookingId id, LeadName leadName, ServiceDescription serviceDescription, Amount salePrice) {
         this.id = id;
+        this.leadName = leadName;
+        this.serviceDescription = serviceDescription;
+        this.salePrice = salePrice;
     }
 
-    public BookingId getId() {
-        return id;
+
+
+    public void update() {
+
     }
+
+    public void cancel() {
+
+    }
+
 }
---
+++ backoffice/modulith/src/main/java/io/mateu/travel/backoffice/booking/domain/model/aggregates/booking/valueobjects/ServiceDescription.java
@@ -0,0 +1,4 @@
+package io.mateu.travel.backoffice.booking.domain.model.aggregates.booking.valueobjects;
+
+public record ServiceDescription(String id) {
+}
